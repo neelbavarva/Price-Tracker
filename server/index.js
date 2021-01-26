e0dcd6585d9adb5ee39d6b6cf166dfee52d48630
neelbavarva <neelbavarva5@gmail.com>
2021-01-27 00:48:28 +0530

    screens

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const content = require('fs').readFileSync(__dirname + '/index.html', 'utf8');
+
+const httpServer = require('http').createServer((req, res) => {
+  // serve the index.html file
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Content-Length', Buffer.byteLength(content));
+  res.end(content);
+});
+
+const io = require('socket.io')(httpServer);
+
+
+io.on('connection', socket => {
+    let counter = 0;
+    setInterval(() => {
+      socket.emit('hello', ++counter);
+    }, 100000);
+  });
+
+httpServer.listen(3000, () => {
+  console.log('go to http://localhost:3000');
+});
